9c64471f0199354c31ff4ffc55107eecd77d1343
wakataw <agungpratama1001@gmail.com>
2022-01-14 07:07:13 +0000

    add course cleanser class

---
+++ classes/Cleanser.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace local_retake;
+
+class Cleanser 
+{
+    public function cleanActivityCompletion($courseId) {
+        echo "clean activity completion for course $courseId";
+    }
+}
