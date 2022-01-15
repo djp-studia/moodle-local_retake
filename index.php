b7056c61129c3af62b6c8c5e59fd15c8cf10d5ac
wakataw <agungpratama1001@gmail.com>
2022-01-15 10:07:37 +0000

    rebuild course cache after activity completion deletion

--- index.php
+++ index.php
@@ -13,6 +13,8 @@ $course = $DB->get_record('course', array('id' => $courseid), '*', MUST_EXIST);
 $context = context_course::instance($course->id);
 $url = new moodle_url('/local/retake/index.php', array('course'=>$course->id));
 $courseUrl = new moodle_url('/course/view.php', array('id' => $courseid));
+
+// set page content
 $PAGE->set_url($url);
 $PAGE->set_pagelayout('standard');
 $PAGE->set_title('Retake Course');
@@ -21,6 +23,14 @@ $PAGE->set_heading("Retake Course $course->fullname");
 // jika request method POST, lakukan penghapusan di sini, lalu redirect ke halaman course
 
 if(isset($_POST['confirm_retake'])){
+    require_once($CFG->dirroot. '/course/lib.php');
+
+    \local_retake\Cleanser::cleanActivityCompletion($courseid, $USER->id);
+
+    // rebuild course cache untuk update tampilan di user
+    rebuild_course_cache($courseid);
+
+    // redirect ke course
     header("Location: $courseUrl");
 }
 
