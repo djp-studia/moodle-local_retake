2d1026f023c9b658d1c312e61a27c99c0261429e
wakataw <agungpratama1001@gmail.com>
2022-01-15 10:08:24 +0000

    add comment and whitespace

--- lib.php
+++ lib.php
@@ -2,6 +2,9 @@
 
 defined('MOODLE_INTERNAL') || die;
 
+/**
+ * Fungsi untuk menambahkan navigasi course retake pada course setting pada setiap course
+ */
 function local_retake_extend_navigation_course($navigation, $course, $context) {
     $url = new moodle_url('/local/retake/index.php', array('course'=>$course->id));
     $navigation->add('Retake Course', $url, navigation_node::TYPE_SETTING, null, null, new pix_icon('i/reload', ''));
