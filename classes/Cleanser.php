bc1348d7072f36be23dd98bce74f49312dddb835
wakataw <agungpratama1001@gmail.com>
2022-01-15 10:08:08 +0000

    implement clean activity completion method

--- classes/Cleanser.php
+++ classes/Cleanser.php
@@ -1,10 +1,38 @@
 <?php
+/**
+* Local Retake course cleanser class
+*
+* @package    local_retake
+* @copyright  2022 Agung Pratama
+* @license    MIT
+*/
 
 namespace local_retake;
 
+defined('MOODLE_INTERNAL') || die();
+
 class Cleanser 
 {
-    public function cleanActivityCompletion($courseId) {
-        echo "clean activity completion for course $courseId";
+    /**
+    * fungsi untuk menghapus seluruh activity completion record dari database berdasarkan ID Course dan User.
+    * @param   int $courseId Course ID
+    * @param   int $userId   User ID
+    * @return  int $data     Database API Status
+    */
+    public function cleanActivityCompletion($courseId, $userId) {
+        global $DB;
+
+        $sql = 'DELETE B
+                FROM {course_modules} A
+                JOIN {course_modules_completion} B ON A.id = B.coursemoduleid
+                WHERE B.userid = ?
+                AND A.course = ?';
+
+        $data = $DB->execute(
+            $sql,
+            [$userId, $courseId]
+        );
+
+        return $data;
     }
 }
