0a7e1484a171b97e948c75997be2d783da047aca
wakataw <agungpratama1001@gmail.com>
2022-01-18 15:19:18 +0700

    add admin page

---
+++ admin.php
@@ -0,0 +1,42 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+require(__DIR__ . '/../../config.php');
+
+// cek apakah user sudah login
+require_login();
+
+// cek parameter course id pada URL
+$courseid = required_param('id', PARAM_INT);
+
+// get data course berdasarkan parameter course id dan user id
+$course = $DB->get_record('course', array('id' => $courseid), '*', MUST_EXIST);
+$context = context_course::instance($course->id);
+$url = new moodle_url('/local/retake/index.php', array('id'=>$course->id));
+$courseUrl = new moodle_url('/course/view.php', array('id' => $courseid));
+
+// set page content
+$PAGE->set_url($url);
+$PAGE->set_context($context);
+$PAGE->set_course($course);
+$PAGE->set_pagelayout('standard');
+$PAGE->set_title('Retake Course');
+$PAGE->set_heading("Retake Course $course->fullname");
+
+// display content
+echo $OUTPUT->header();
+echo 'This is admin page';
+echo $OUTPUT->footer();
