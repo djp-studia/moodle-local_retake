5dc132be129bc0b66295bb901c9e9daffcf4ef20
wakataw <agungpratama1001@gmail.com>
2022-01-18 15:19:03 +0700

    add admin navigation

--- lib.php
+++ lib.php
@@ -20,6 +20,13 @@ defined('MOODLE_INTERNAL') || die;
  * Fungsi untuk menambahkan navigasi course retake pada course setting pada setiap course
  */
 function local_retake_extend_navigation_course($navigation, $course, $context) {
-    $url = new moodle_url('/local/retake/index.php', array('id'=>$course->id));
+    $resetCapability = has_capability('moodle/course:reset', $context);
+
+    if($resetCapability){
+        $url = new moodle_url('/local/retake/admin.php', array('id' => $course->id));
+    } else {
+        $url = new moodle_url('/local/retake/index.php', array('id' => $course->id));
+    }
+
     $navigation->add('Retake Course', $url, navigation_node::TYPE_SETTING, null, null, new pix_icon('i/reload', ''));
 }
